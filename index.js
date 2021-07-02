f51d39b0302c41fd42b013026586cdac16f43912
StarkovAleksandr <starkovlife@gmail.com>
2021-07-02 16:26:38 +0300

    First_Project

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-for (let i = 0; i < array.length; i++) {
-    if (array[i]%2 !== 0){
-        array.splice(i, 1);
-    }
+let table = document.querySelector("#table");
+
+let k = 1;
+for (let i = 0; i < 6; i++) {
+  let tr = document.createElement("tr");
+
+  for (let i = 0; i < 6; i++) {
+    let td = document.createElement("td");
+
+    td.innerHTML = k;
+    k++;
+
+    tr.appendChild(td);
+  }
+
+  table.appendChild(tr);
 }
-
-console.log(array)
-for (let i = 0; i < array.length; i++) {
-        array[i] = array[i] * 2;
-        console.log(array[i]);
-}
-
-
