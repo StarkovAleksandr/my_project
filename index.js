6c96e4352f942cf61e9fd4d5f5dbc8f34da91a25
StarkovAleksandr <starkovlife@gmail.com>
2021-07-02 18:19:53 +0300

    Итоговый вариант таблицы

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 let table = document.querySelector("#table");
 
 let k = 1;
+
+let trHead = document.createElement("tr");
+let tdHead = document.createElement("td");
+
+tdHead.innerHTML = "Эта красивая таблица специально для тебя!!!";
+tdHead.colSpan = 6;
+
+trHead.appendChild(tdHead);
+table.appendChild(trHead);
+
 for (let i = 0; i < 6; i++) {
   let tr = document.createElement("tr");
 
