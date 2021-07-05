05813aa036b8407ddd7f8bc91e61032d83ae3465
StarkovAleksandr <starkovlife@gmail.com>
2021-07-05 15:17:27 +0300

    Изменена работа программы "Task2"

--- HomeTasks2/Task2.js
+++ HomeTasks2/Task2.js
@@ -1,6 +1,8 @@
-let arr = [57, 157, 230];
+let arr = [57, 157, 230, 5];
 function removeMinimal(arr) {
   var min = Math.min.apply(null, arr);
-  return min;
+  return arr.filter((e) => {
+    return e != min;
+  });
 }
 console.log(removeMinimal(arr));
