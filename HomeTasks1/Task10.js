f0aaab107131d60cb668962f25eead460d543cb3
StarkovAleksandr <starkovlife@gmail.com>
2021-07-07 17:56:36 +0300

    Готово 10 задание

---
+++ HomeTasks1/Task10.js
@@ -0,0 +1,6 @@
+console.log(5 + "2");
+console.log((5 + "2" == "5" + "2") == "52");
+console.log(5 - "2");
+console.log((5 - "2" == 5 - 2) == 3);
+console.log(true + true);
+console.log((true + true == 1 + 1) == 2);
